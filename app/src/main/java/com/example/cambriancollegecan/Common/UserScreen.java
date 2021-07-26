66eb0dfad1801aad01ace7139f2671155ee51c86
Uditha <uditha_a@live.com>
2021-07-25 22:26:05 -0400

    final layout of options screen

--- app/src/main/java/com/example/cambriancollegecan/Common/UserScreen.java
+++ app/src/main/java/com/example/cambriancollegecan/Common/UserScreen.java
@@ -2,17 +2,43 @@ package com.example.cambriancollegecan.Common;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.WindowManager;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.cambriancollegecan.R;
 
 public class UserScreen extends AppCompatActivity {
 
+    public ImageButton guestUser;
+    public ImageButton registeredUser;
+
+    public void GuestUser(){
+        guestUser = (ImageButton)findViewById(R.id.guest_user);
+        guestUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent guest = new Intent(UserScreen.this,OptionScreen.class);
+                startActivity(guest);
+            }
+        });
+    }
+    public void registeredUser(){
+        registeredUser = (ImageButton)findViewById(R.id.registered_user);
+        registeredUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent register = new Intent(UserScreen.this,OptionScreen.class);
+                startActivity(register);
+            }
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_user_screen);
+        setContentView(R.layout.user_screen);
+        GuestUser();
+        registeredUser();
     }
 }
