5e3e8d3eb2932407952296bf6e6f1e43052b94dd
Uditha <uditha_a@live.com>
2021-08-20 14:22:23 -0400

    Apply Now

--- app/src/main/java/com/example/cambriancollegecan/Health/HeHealthAnalytics.java
+++ app/src/main/java/com/example/cambriancollegecan/Health/HeHealthAnalytics.java
@@ -2,15 +2,33 @@ package com.example.cambriancollegecan.Health;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.cambriancollegecan.R;
+import com.example.cambriancollegecan.User.LoginScreen;
 
 public class HeHealthAnalytics extends AppCompatActivity {
+
+    public Button applyNow;
+
+    public void ApplyNow(){
+        applyNow = (Button) findViewById(R.id.apply_now);
+        applyNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent guest = new Intent(HeHealthAnalytics.this, LoginScreen.class);
+                startActivity(guest);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.he_health_analytics);
+        ApplyNow();
     }
 }
--- app/src/main/java/com/example/cambriancollegecan/Health/HeMedicalSono.java
+++ app/src/main/java/com/example/cambriancollegecan/Health/HeMedicalSono.java
@@ -2,15 +2,33 @@ package com.example.cambriancollegecan.Health;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.cambriancollegecan.R;
+import com.example.cambriancollegecan.User.LoginScreen;
 
 public class HeMedicalSono extends AppCompatActivity {
+
+    public Button applyNow;
+
+    public void ApplyNow(){
+        applyNow = (Button) findViewById(R.id.apply_now);
+        applyNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent guest = new Intent(HeMedicalSono.this, LoginScreen.class);
+                startActivity(guest);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.he_medical_sono);
+        ApplyNow();
     }
 }
